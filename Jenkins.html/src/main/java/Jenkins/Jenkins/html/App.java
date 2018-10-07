f7deb10f31950cc859b17bb68770d49087a74802
mushtaqsayyed <43338423+mushtaqsayyed@users.noreply.github.com>
2018-10-07 21:26:09 +0530

    a

---
+++ Jenkins.html/src/main/java/Jenkins/Jenkins/html/App.java
@@ -0,0 +1,15 @@
+package Jenkins.Jenkins.html;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        AppTest a = new AppTest();
+    a.myFile();
+   
+    }
+}
